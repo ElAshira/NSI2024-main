07a13d91d35aba795744196b662ea9fa03a1ef83
ElAshira <pierre-antoine.soubrier@student-oec.fr>
2023-10-20 14:54:02 +0200

    Add a file_level to check the actual level New window when launching game New class collisionneur

--- MicroProject/molecule.py
+++ MicroProject/molecule.py
@@ -5,11 +5,13 @@ class Molecule:
         self.x = x
         self.y = y
         self.image = pygame.image.load(image)
+        self.can_move = True
 
     def move(self,x,y):
         self.x += x
         self.y += y
-
+    def get_image(self):
+        return self.image
     def get_position(self):
         return (self.x, self.y)
 
@@ -17,3 +19,13 @@ class Molecule:
         self.x = x
         self.y = y
 
+
+class Colisionneur:
+    def __init__(self, x,y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("Assets\\molecule\\block.png")
+        self.can_move = False
+    def set_position(self,x,y):
+        self.x = x
+        self.y = y
